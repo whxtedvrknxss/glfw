883cca84048dfd6abc7f72f2588b9cc499369456
whxtedvrknxss <kapattu333@gmail.com>
2025-05-16 00:32:12 +0100

    fix

--- premake5.lua
+++ premake5.lua
@@ -9,30 +9,17 @@ project "glfw"
   {
     "include/GLFW/glfw3.h",
     "include/GLFW/glfw3native.h",
-    "src/glfw_config.h",
-    "src/context.c",
-    "src/init.c",
-    "src/input.c",
-    "src/monitor.c",
-    "src/vulkan.c",
-    "src/window.c"
+    "src/*.c"
   }
 
   filter "system:windows"
     systemversion "latest"
     staticruntime "On"
 
-    files 
+    defines 
     {
-      "src/win32_init.",
-      "src/win32_joystick.c",
-      "src/win32_monitor.c",
-      "src/win32_time.c",
-      "src/win32_thread.c",
-      "src/win32_window.c",
-      "src/wgl_context.c",
-      "src/egl_context.c",
-      "src/osmesa_context.c"
+      "_GLFW_WIN32",
+      "_CRT_SECURE_NO_WARNINGS"
     }
 
   filter "configurations:Debug"
